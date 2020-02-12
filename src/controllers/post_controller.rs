1e15bacd39170debb1011491a75d70f3a1e2ad37
Justin Parado <justin.parado@dice205.com>
2020-02-13 05:00:28 +0800

    Moved Post demo routes underneath, in their own section.

--- src/controllers/post_controller.rs
+++ src/controllers/post_controller.rs
@@ -41,24 +41,6 @@ pub async fn store(
     .map_err(|_| HttpResponse::InternalServerError())
 }
 
-#[post("/demo-store")]
-pub async fn demo_store(pool: StatePool, form: web::Json<NewPost>) -> impl Responder {
-    use crate::schema::posts::dsl::*;
-    use diesel::prelude::*;
-    let conn = &pool.get().unwrap();
-    let new_post: NewPost = NewPost {
-        title: form.title.to_owned(),
-        body: form.body.to_owned(),
-        user_id: form.user_id.to_owned(),
-    };
-    let create_post_res = diesel::insert_into(posts)
-        .values(new_post)
-        .get_result::<Post>(conn);
-    match create_post_res {
-        Ok(created_post) => HttpResponse::Ok().json(Single { post: created_post }),
-        Err(_) => HttpResponse::InternalServerError().json("Error"),
-    }
-}
 
 #[delete("/{id}")]
 pub async fn destroy(
@@ -74,19 +56,6 @@ pub async fn destroy(
         })
 }
 
-#[delete("/demo-delete/{id}")]
-pub async fn demo_destroy(pool: StatePool, path: web::Path<IdPath>) -> impl Responder {
-    use crate::schema::posts::dsl::*;
-    use diesel::prelude::*;
-    let target = posts.find(&path.id);
-    let conn = &pool.get().unwrap();
-    let res = diesel::delete(target).get_result::<Post>(conn);
-
-    match res {
-        Ok(deleted_post) => HttpResponse::Ok().json(Single { post: deleted_post }),
-        Err(e) => HttpResponse::InternalServerError().json("Error")
-    }
-}
 
 #[patch("/{id}")]
 pub async fn update(
@@ -114,6 +83,41 @@ pub async fn show(pool: StatePool, path: web::Path<IdPath>, auth_user: AuthUser)
         .await
         .map(|post| HttpResponse::Ok().json(Single { post }))
         .map_err(|_| HttpResponse::InternalServerError())
+}
+
+
+// Demo Routes (no auth required)
+#[post("/demo-store")]
+pub async fn demo_store(pool: StatePool, form: web::Json<NewPost>) -> impl Responder {
+    use crate::schema::posts::dsl::*;
+    use diesel::prelude::*;
+    let conn = &pool.get().unwrap();
+    let new_post: NewPost = NewPost {
+        title: form.title.to_owned(),
+        body: form.body.to_owned(),
+        user_id: form.user_id.to_owned(),
+    };
+    let create_post_res = diesel::insert_into(posts)
+        .values(new_post)
+        .get_result::<Post>(conn);
+    match create_post_res {
+        Ok(created_post) => HttpResponse::Ok().json(Single { post: created_post }),
+        Err(_) => HttpResponse::InternalServerError().json("Error"),
+    }
+}
+
+#[delete("/demo-delete/{id}")]
+pub async fn demo_destroy(pool: StatePool, path: web::Path<IdPath>) -> impl Responder {
+    use crate::schema::posts::dsl::*;
+    use diesel::prelude::*;
+    let target = posts.find(&path.id);
+    let conn = &pool.get().unwrap();
+    let res = diesel::delete(target).get_result::<Post>(conn);
+
+    match res {
+        Ok(deleted_post) => HttpResponse::Ok().json(Single { post: deleted_post }),
+        Err(e) => HttpResponse::InternalServerError().json("Error"),
+    }
 }
 
 #[derive(Serialize, Deserialize)]
